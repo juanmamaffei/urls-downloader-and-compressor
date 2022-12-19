4a48ba669ef0d669dfb7670c168852e4a180ea3a
Juan Manuel MAffei <juanmamaffei@gmail.com>
2022-12-19 15:32:21 -0300

    feat

---
+++ app/services/enqueue_compression.rb
@@ -0,0 +1,16 @@
+class EnqueueCompression 
+  def initialize(files_string)
+    @files_string = files_string
+  end
+
+  def run
+    # Parse string as array of file urls
+
+    # Create Petition and related Files
+
+    # Create CompressionJob
+
+    # Return URL to follow for status
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'files/run'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "main#index"
+
+  # Defines the route for the "run" action in the "files" controller
+  post "compress", to: "files#run"
 end
