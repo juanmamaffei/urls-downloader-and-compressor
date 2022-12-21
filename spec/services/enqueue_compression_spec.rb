0dfa6b567fbaf4131dd0a22f6941b32745bdff87
Juan Manuel MAffei <juanmamaffei@gmail.com>
2022-12-21 12:24:43 -0300

    feat: url parser added

---
+++ spec/services/enqueue_compression_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe EnqueueCompression do
+  context "when receives a valid list of files" do
+    it "returns a URL" do
+    end
+  end
+
+  context "when receives invalid params" do
+    it "returns an error" do
+    end
+  end
+end
