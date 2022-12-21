6aaa2c2ced3dd5c7bb0355039e97e8bdfbf94a4c
Juan Manuel MAffei <juanmamaffei@gmail.com>
2022-12-21 18:51:49 -0300

    feat: petition creation added to service

--- app/services/enqueue_compression.rb
+++ app/services/enqueue_compression.rb
@@ -6,11 +6,26 @@ class EnqueueCompression
   def run
     # Parse string as array of file urls
     parsed_urls = UrlParserUtil.new.parse(@files_string)
+
     # Create Petition and related Files
+    create_petition
+    assign_files(parsed_urls)
 
     # Create CompressionJob with petition_id
 
     # Return URL to follow for status
 
+  end
+
+  private
+
+  def create_petition
+    @petition = Petition.create(status: 'pending')
+  end
+
+  def assign_files(urls)
+    urls.each do |url|
+      @petition.subfiles.create(url: url, status: 'pending')
+    end
   end
 end
